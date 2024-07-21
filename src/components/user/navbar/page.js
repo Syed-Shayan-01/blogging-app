b2afa714a1272aec7a84b0b36c8f16857f5ef342
shayansyed712 <shayansyed712@gmail.com>
2024-07-21 05:34:32 +0000

    new change

--- src/components/user/navbar/page.js
+++ src/components/user/navbar/page.js
@@ -1,66 +1,42 @@
 'use client'
-import Link from "next/link";
 import { useState } from "react";
-import { IoIosSearch } from "react-icons/io";
+import { GrMenu } from "react-icons/gr";
+import { IoCloseSharp } from "react-icons/io5";
+export default function Navbar() {
+    const [MenuOpen, isMenuOpen] = useState(false);
 
-const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const toggleIcon = () => {
+        isMenuOpen(!MenuOpen)
+    }
     return (
-        <>
-            <header className="bg-white shadow">
-                <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-                    <div className="text-2xl font-bold">MetaBlog</div>
-                    <div className="block lg:hidden">
-                        <button
-                            onClick={() => setIsOpen(!isOpen)}
-                            className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
-                        >
-                            <svg
-                                className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                            </svg>
-                            <svg
-                                className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <nav className={`flex space-x-4 ${isOpen ? "block" : "hidden"}`}>
-
-                        <Link href="/" className="hover:text-blue-500">
-                            Home
-                        </Link>
-                        <Link href="#" className="hover:text-blue-500">
-                            Blog
-                        </Link>
-                        <Link href="/user/singleblog/" className="hover:text-blue-500">
-                            Single Post
-                        </Link>
-                        <Link href="#" className="hover:text-blue-500">
-                            Pages
-                        </Link>
-                        <Link href="#" className="hover:text-blue-500">
-                            Contact
-                        </Link>
-
-                        <div className="flex space-x-4">
-                            <input type="text" placeholder="Search" className="border rounded px-2 py-1" />
-                            <button className="p-2 rounded-full bg-gray-200 hover:bg-gray-300">
-                                <IoIosSearch className="h-6 w-6 text-gray-700" />
-                            </button>
-                        </div> </nav>
-                </div>
-            </header >
-        </>
-    )
+        <header className="shadow">
+            <div
+                className="relative flex justify-between max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
+                <a href="/" className="flex items-center whitespace-nowrap text-2xl font-black">
+                    <span className="mr-2 text-4xl text-blue-600">
+                        <span className="text-[1.7rem] text-blue-600"><i className="fa-solid fa-dumbbell"></i></span>
+                    </span>
+                    <span className="text-black text-[2rem]">My Blogee</span>
+                </a>
+                <input type="checkbox" className="peer hidden" id="navbar-open" />
+                <label onClick={toggleIcon} className="absolute top-6 right-7 cursor-pointer md:hidden" htmlFor="navbar-open">
+                    <span className="sr-only">Toggle Navigation</span>
+                    {!MenuOpen && <span className="text-[1.2rem] hover:bg-blue-600 "><GrMenu /></span>}
+                    {MenuOpen && <span span className="text-[1.2rem] hover:bg-blue-600 "><IoCloseSharp /></span>}
+                </label>
+                <nav aria-label="Header Navigation"
+                    className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
+                    <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
+                        <li className="text-gray-600 md:mr-12 text-[1.2rem] py-1 hover:text-blue-600"><a href="/">Home</a>
+                        </li>
+                        <li className="text-gray-600 md:mr-12 text-[1.2rem] py-1 hover:text-blue-600"><a href="/user/blogs/">Blogs</a>
+                        </li>
+                        <li className="text-gray-600 md:mr-12 text-[1.2rem] py-1 hover:text-blue-600">
+                            <a href="/contact.html">Contact us</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header >
+    );
 }
-
-export default Navbar
-
-
